2ed1a54ac978b784752d8798dac1156dd4b8ae52
yash tyagi <78555443+yash9953@users.noreply.github.com>
2023-03-25 06:00:33 +0530

    Add files via upload

---
+++ InverrtedMirrorRightTriangle.java
@@ -0,0 +1,38 @@
+package PatternUsingRecursion;
+
+import java.util.Scanner;
+
+public class InverrtedMirrorRightTriangle {
+          // 5
+          // *****
+          //  ****
+          //   ***
+          //    **
+          //     *
+          public static void row(int n,int i){
+                    if(i<=n){
+                              colspace(n,i,1);
+                              col(n, i, 1);
+                              System.out.println();
+                              row(n, i+1);
+                    }
+          }
+          public static void colspace(int n,int i,int j){
+                    if(j<i&&i>=2){
+                              System.out.print(" ");
+                              colspace(n, i, j+1);
+                    }
+          }
+          public static void col(int n,int i,int j){
+                    if(j<=n-i+1){
+                              System.out.print("*");
+                              col(n, i, j+1);
+                    }
+          }
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    int n=sc.nextInt();
+                    row(n,1);
+                    sc.close();
+          }
+}
---
+++ InvertedPyramid.java
@@ -0,0 +1,52 @@
+package PatternUsingRecursion;
+
+import java.util.Scanner;
+
+public class InvertedPyramid {
+          // 5
+          // *********
+          //  *******
+          //   *****
+          //    ***
+          //     *
+          public static void row(int n,int i){
+                    if(i<=n){
+                              space1(n,i,1);
+                              col1(n,i,1);
+                              col2(n,i,1);
+                              space2(n,i,1);
+                              System.out.println();
+                              row(n,++i);
+                    }
+          } 
+          public static void space1(int n,int i,int j){
+                    if(i>=2&&j<=i-1){
+                              System.out.print(" ");
+                              space1(n, i, ++j);
+                    }
+          }
+          public static void col1(int n,int i,int j){
+                    if(j<=n-i+1){
+                              System.out.print("*");
+                              col1(n, i, ++j);
+                    }
+          }
+          public static void col2(int n,int i,int j){
+                    if(j<=n-i){
+                              System.out.print("*");     
+                              col2(n, i, ++j);
+                    }
+          }
+          public static void space2(int n,int i,int j){
+                    if(i>=2&&j<=i-1){
+                              System.out.print(" ");
+                              space2(n, i, ++j);
+                    }
+          }
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    int n=sc.nextInt();
+                    row(n,1);
+                    sc.close();
+          }
+}
