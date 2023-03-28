30e13ff6ad286cc69690a30923096c15e6be4061
Yash Tyagi <yashtyagi9953@gmail.com>
2023-03-28 06:29:34 +0530

    Add files via upload

---
+++ RohmbusStarPattern.java
@@ -0,0 +1,38 @@
+package PatternUsingRecursion;
+
+import java.util.Scanner;
+
+public class RohmbusStarPattern {
+          // 5
+          //     *****
+          //    *****
+          //   *****
+          //  *****
+          // *****
+          public static void row(int n,int i){
+                    if(i<=n){
+                              space(n,i,1);
+                              col(n,i,1);
+                              System.out.println();
+                              row(n, ++i);
+                    }
+          }
+          public static void space(int n,int i,int j){
+                    if(j<=n-i){
+                              System.out.print(" ");
+                              space(n, i,++j);
+                    }
+          }
+          public static void col(int n,int i,int j){
+                    if(j<=n){
+                              System.out.print("*");
+                              col(n, i, ++j);
+                    }
+          }
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    int n=sc.nextInt();
+                    row(n,1);
+                    sc.close();
+          }
+}
